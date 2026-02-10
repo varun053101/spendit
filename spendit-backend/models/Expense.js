f3f03a8f38b0d491559f6b863a986d48a6418be3
varun053101 <varun05310104@gmail.com>
2026-02-10 12:12:35 +0530

    Created database schema

---
+++ spendit-backend/models/Expense.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const expenseSchema = new mongoose.Schema(
+  {
+    amount: {
+      type: Number,
+      required: true
+    },
+
+    category: {
+      type: String,
+      required: true
+    },
+
+    date: {
+      type: Date,
+      required: true
+    },
+
+    description: {
+      type: String,
+    },
+
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model("Expense", expenseSchema);
---
+++ spendit-backend/models/User.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true
+    },
+
+    password: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+// Hash before saving to the database
+userSchema.pre('save', async function(next){
+    const user = this;
+
+    // Hash the password only if it has been modified or its new record
+    if(!user.isModified('password'))   return next();
+    try{
+        //Generate hash password(salting)
+        const salt = await bcrypt.genSalt(10);
+
+        // Hash the password
+        const hashedPassword = await bcrypt.hash(user.password, salt);
+
+        // Override the plain password with the hashed one
+        user.password = hashedPassword;
+
+        next();
+    }catch(err){
+        return next(err);
+    }
+});
+
+userSchema.methods.comparePassword = async function(userPassword){
+    try {
+        // use bcrypt.compare() to compare the provided password with the hashed password
+        const isMatch = await bcrypt.compare(userPassword, this.password);
+        return isMatch;
+    } catch(err) {
+        throw err;
+    }
+}
+
+module.exports = mongoose.model("User", userSchema);
