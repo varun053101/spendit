e2ad2250e778f1fbd5f90f070282224da676c5c9
varun053101 <varun05310104@gmail.com>
2026-02-10 22:43:53 +0530

    hanlded CORS issues

--- spendit-backend/server.js
+++ spendit-backend/server.js
@@ -3,8 +3,14 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const db = require('./database/db')
+const cors = require('cors');
 
 app.use(express.json())
+
+app.use(cors({
+  origin: process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : [],
+  credentials: true
+}));
 
 const userRoutes = require("./routes/userRoutes")
 const expenseRoutes = require("./routes/expenseRoutes")
